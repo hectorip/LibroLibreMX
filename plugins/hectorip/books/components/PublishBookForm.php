2e1a90e1c03efa578233857327a3465259ea37e0
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-11-02 00:57:50 -0600

    Adding a Book By User

--- plugins/hectorip/books/components/PublishBookForm.php
+++ plugins/hectorip/books/components/PublishBookForm.php
@@ -2,6 +2,8 @@
 
 use Cms\Classes\ComponentBase;
 use Hectorip\Books\Models\Book;
+use RainLab\User\Classes\AuthManager as RainAuthManager;
+
 
 class PublishBookForm extends ComponentBase {
 
@@ -19,6 +21,9 @@ class PublishBookForm extends ComponentBase {
 		$newBook = new Book;
 
 		$newBook->title = post('book_title');
+		$auth           = RainAuthManager::instance();
+		// var_dump(\Session::all());
+		$newBook->user_id = $auth->getUser()->user_details->id;
 
 		$newBook->save();
 
