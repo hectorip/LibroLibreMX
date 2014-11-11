45776c9573488a6502e10d3466a94ece736966b8
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-11-10 23:59:32 -0600

    Starting orders

---
+++ plugins/hectorip/books/updates/create_orders_table.php
@@ -0,0 +1,28 @@
+<?php namespace Hectorip\Books\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateOrdersTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('hectorip_books_orders', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->int('book_id');
+            $table->int('user_to');
+            $table->text('comments');
+            $table->bool('fulfilled');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('hectorip_books_orders');
+    }
+
+}
